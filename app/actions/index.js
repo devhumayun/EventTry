43054e3322b55022304a44df7f9b38141a43dfba
devhumayun <humayunkabir9408@gmail.com>
2024-04-24 08:40:05 +0600

    there is an error to sending mail

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,11 +2,13 @@
 import {
   createUser,
   findUserByCredentials,
+  getEventById,
   updateGoing,
   updateInterest,
 } from "@/db/queries";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { Resend } from "resend";
 
 export const registerUser = async (formData) => {
   const userData = Object.fromEntries(formData);
@@ -40,8 +42,25 @@ export const addOrRemoveInterestedEvent = async (eventId, authId) => {
 export const addGoingToEvent = async (eventId, user) => {
   try {
     await updateGoing(eventId, user?.id);
+    await sendMail(eventId, user);
     revalidatePath("/");
     redirect("/");
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const sendMail = async (eventId, user) => {
+  try {
+    const event = await getEventById(eventId);
+    const resend = new Resend(process.env.RESEND_MAIL_API_KEY);
+    const message = `Dear ${user?.name}, you have successfully registered for the event, ${event?.name}, please carry this mail and official id to the venue. We are excited to have you here`;
+    const sent = await resend.emails.send({
+      from: "humayun@gmail.com",
+      to: user?.mail,
+      subject: "Successfully Registered for the event",
+      html: <p>{message}</p>,
+    });
   } catch (error) {
     throw error;
   }

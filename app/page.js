23e795bd312139c762d8719c5544d1cc1cbe8b7f
devhumayun <humayunkabir9408@gmail.com>
2024-04-24 12:13:08 +0600

    search implement using debounce hooks

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Header from "@/components/common/Header";
 import EventList from "@/components/event/EventList";
 
-export default function Home() {
+export default function Home({ searchParams: { query } }) {
   return (
     <section className="container">
       <Header />
-      <EventList />
+      <EventList query={query} />
     </section>
   );
 }
